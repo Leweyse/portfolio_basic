ec23fb56560803c5a4b3207603da8f66b2880ea5
Leweyse <darylcastro67@gmail.com>
2021-05-06 22:25:41 +0200

    First version

--- index.js
+++ index.js
@@ -1,50 +1,12 @@
 const link = document.querySelectorAll('#navbar .navbar_section .hover-this');
-
-const animate_link = function (e) {
-    const span = this.querySelectorAll('#navbar span');
-    const { offsetX: x, offsetY: y } = e,
-    { offsetWidth: width, offsetHeight: height } = this,
-
-    move = 20,
-    xMove = (x / width * (move * 2) - move),
-    yMove = (y / height * (move * 2) - move);
-
-    for (var i = 0; i < span.length; i++) {
-      span[i].style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-      if (e.type === 'mouseleave') span[i].style.transform = '';
-    }
-};
-
-link.forEach(b => b.addEventListener('mousemove', animate_link));
-link.forEach(b => b.addEventListener('mouseleave', animate_link));
-
-
-
-// Detect request animation frame
-const scroll = window.requestAnimationFrame ||
-             // IE Fallback
-             function(callback){ window.setTimeout(callback, 1000/60)};
 const elementsToShow = document.querySelectorAll('.show-on-scroll');
 const plink = document.querySelectorAll('.profile-link');
 const alink = document.querySelectorAll('.project-tile_alink');
 
 const all_e = [...elementsToShow, ...plink, ...alink];
 
-function loop() {
-    Array.prototype.forEach.call(all_e, function(element){
-      if (isElementInViewport(element)) {
-        element.classList.add('is-visible');
-      } else {
-        element.classList.remove('is-visible');
-      }
-    });
-
-    scroll(loop);
-}
-
-// Call the loop for the first time
-loop();
+// Detect request animation frame
+const scroll = window.requestAnimationFrame || function(callback) { window.setTimeout(callback, 1000/60) };
 
 // Helper function from: http://stackoverflow.com/a/7557433/274826
 function isElementInViewport(el) {
@@ -64,3 +26,35 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
+
+// Call the loop for the first time
+loop();
+
+function loop() {
+  Array.prototype.forEach.call(all_e, function(element){
+    if (isElementInViewport(element)) element.classList.add('is-visible');
+    else element.classList.remove('is-visible');
+  });
+
+  scroll(loop);
+}
+
+//Button "About Me "
+const animate_link = function (e) {
+    const span = this.querySelectorAll('#navbar span');
+    const { offsetX: x, offsetY: y } = e,
+    { offsetWidth: width, offsetHeight: height } = this,
+
+    move = 20,
+    xMove = (x / width * (move * 2) - move),
+    yMove = (y / height * (move * 2) - move);
+
+    for (var i = 0; i < span.length; i++) {
+      span[i].style.transform = `translate(${xMove}px, ${yMove}px)`;
+
+      if (e.type === 'mouseleave') span[i].style.transform = '';
+    }
+};
+
+link.forEach(b => b.addEventListener('mousemove', animate_link));
+link.forEach(b => b.addEventListener('mouseleave', animate_link));
